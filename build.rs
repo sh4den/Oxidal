b4337543ad954c86be10599155b6b4b981c6cbb2
Clément Ranouch <69421356+Clems54120@users.noreply.github.com>
2026-07-24 16:12:57 +0200

    feat(packaging): add hyperland compatibility

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("cargo:rerun-if-changed=packaging/windows/icon.ico");
+    #[cfg(windows)]
     if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows") {
         let mut res = winresource::WindowsResource::new();
         res.set_icon("packaging/windows/icon.ico");
